55f09533a689e5f6ea85694f9332c3a96ba675f5
SHRESHTH GUPTA <107746269+KINGUSSS2627@users.noreply.github.com>
2023-01-01 01:34:30 +0530

    Add files via upload
    https://codeforces.com/problemset/problem/124/A

---
+++ 1000/A_The_number_of_positions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define tc while (t--)
+#define in cin >>
+#define out cout <<
+#define ret0 return 0;
+using namespace std;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, n;
+    in n >> a >> b;
+    out n - max(a + 1, n - b) + 1;
+    ret0
+}
